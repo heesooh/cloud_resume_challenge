9283071dde59e37f19207305762888169ea6eecb
heesooh <45409054+heesooh@users.noreply.github.com>
2026-03-04 12:56:42 -0800

    feat: Implemented API gateway that integrates Lambda function to update the DynamoDB visitor count.

--- infra/main.tf
+++ infra/main.tf
@@ -99,3 +99,28 @@ resource "aws_lambda_function" "visitor_count_lambda" {
     handler = "lambda.lambda_handler"
     runtime = "python3.14"
 }
+
+# API Gateway
+resource "aws_apigatewayv2_api" "visitor_count_api" {
+    name          = "cloud_resume_challenge_tf_api"
+    protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "visitor_count_api_lambda" {
+    api_id = aws_apigatewayv2_api.visitor_count_api.id
+    integration_type = "AWS_PROXY"
+    integration_uri = aws_lambda_function.visitor_count_lambda.invoke_arn
+    payload_format_version  = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "visitor_count_api_route" {
+    api_id    = aws_apigatewayv2_api.visitor_count_api.id
+    route_key = "GET /count"
+    target    = "integrations/${aws_apigatewayv2_integration.visitor_count_api_lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "visitor_count_api_stage" {
+    api_id = aws_apigatewayv2_api.visitor_count_api.id
+    name   = "$default"
+    auto_deploy = true 
+}
